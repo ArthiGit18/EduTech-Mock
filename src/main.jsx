73f022dd9dceda2a92604f913f64669d4b31a5dd
ArthiGit18 <arthiar180@gmail.com>
2025-08-11 15:50:36 +0530

    updated nav actions

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './index.scss'
 import App from './App.jsx'
+import { AuthModalProvider } from './context/AuthModalContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthModalProvider>
+      <App />
+    </AuthModalProvider>
   </StrictMode>,
 )
+
